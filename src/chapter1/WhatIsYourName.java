b0de5ea18c0433c17794a2ba9d13ae81b583ac6c
Aleksandr <sashagetun@gmail.com>
2022-12-21 15:14:27 +0100

    Java1

---
+++ src/chapter1/WhatIsYourName.java
@@ -0,0 +1,16 @@
+package chapter1;
+
+import javax.swing.*;
+import java.util.Scanner;
+
+public class WhatIsYourName {
+
+    public static void main(String[] args) {
+        String name;
+        name = JOptionPane.showInputDialog("Добрый день! Как Вас зовут?");
+        JOptionPane.showMessageDialog(null, "Приятно познакомиться,\n"+name+"!");
+
+
+    }
+
+}
